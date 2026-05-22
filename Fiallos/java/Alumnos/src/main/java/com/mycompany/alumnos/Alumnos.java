15513fd026413068e18970925bcfb8cf9c2b7917
jiry1723 <jiry1723@gmail.com>
2026-05-22 16:33:47 -0500

    si
    agregue dos vectores y le puse valor
    despues lo imprimi

--- Fiallos/java/Alumnos/src/main/java/com/mycompany/alumnos/Alumnos.java
+++ Fiallos/java/Alumnos/src/main/java/com/mycompany/alumnos/Alumnos.java
@@ -12,5 +12,8 @@ public class Alumnos {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Vector2D v1 = new Vector2D();
+        v1.x = 3.0;
+        System.out.println(v1.x);
     }
 }
---
+++ Fiallos/java/Alumnos/src/main/java/com/mycompany/alumnos/Vector2D.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.alumnos;
+
+/**
+ *
+ * @author ricfiall
+ */
+public class Vector2D{
+    public double x;
+    public double y;
+
+    /*public Vector2D(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Vector2D(double x){
+        this.x = x;
+        
+    }*/
+    
+}
