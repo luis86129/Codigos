75779075cf1e964d9568923f4c75535e64667e4d
Luis Mendoza <luismendoza86129@gmail.com>
2026-05-21 21:31:41 -0500

    cree un proyecto en java

---
+++ Luis/Java/SistemaUsuarios/src/main/java/com/mycompany/sistemausuarios/SistemaUsuarios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemausuarios;
+
+/**
+ *
+ * @author Luis Mendoza
+ */
+public class SistemaUsuarios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
