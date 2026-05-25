fb1c5fc88b1893b1686003c3dfbc22e38b48bcbf
Luis Mendoza <luismendoza86129@gmail.com>
2026-05-25 18:02:06 -0500

    cree proyecto en java sobre algoritmos

---
+++ Luis/Java/Algoritmos/src/main/java/com/mycompany/algoritmos/Algoritmos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmos;
+
+/**
+ *
+ * @author Luis Mendoza
+ */
+public class Algoritmos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
