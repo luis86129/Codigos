c76c678042c24b407ece396480dbbd0edcf300f7
Luis Mendoza <luismendoza86129@gmail.com>
2026-05-28 17:05:35 -0500

    bases

---
+++ Luis/Java/LoginUs/src/main/java/com/mycompany/loginus/LoginUs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginus;
+
+/**
+ *
+ * @author Luis Mendoza
+ */
+public class LoginUs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
