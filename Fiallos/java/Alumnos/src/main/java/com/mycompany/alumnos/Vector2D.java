28043bc6aaf93bc138d724cfd6ce01f311fa086d
jiry1723 <jiry1723@gmail.com>
2026-05-29 14:32:32 -0500

    1234

--- Fiallos/java/Alumnos/src/main/java/com/mycompany/alumnos/Vector2D.java
+++ Fiallos/java/Alumnos/src/main/java/com/mycompany/alumnos/Vector2D.java
@@ -12,14 +12,44 @@ public class Vector2D{
     public double x;
     public double y;
 
-    /*public Vector2D(double x, double y){
+    public Vector2D(double x, double y){
         this.x = x;
         this.y = y;
     }
     
     public Vector2D(double x){
         this.x = x;
+        this.y = 0;
         
-    }*/
+    }
+
+    public double getX(){
+        return x;
+    }
     
+    public double getY(){
+        return y;
+    }
+    
+    public void setX(double x){
+        this.x = x;
+        
+    }
+    
+    public void setY(double y){
+        this.y = y;
+    }
+    
+    public Vector2D sumar(Vector2D other){
+        double newX = this.x + other.x;
+        double newY = this.y + other.y;
+        return new Vector2D(newX, newY);
+        
+    }
+    
+    public Vector2D restar(Vector2D other){
+        double newX = this.x - other.x;
+        double newY = this.y - other.y;
+        return new Vector2D(newX, newY);
+    }
 }
