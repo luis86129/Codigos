d443328a9f8f98c4da12cdd66d0fb118b0d8af55
Luis Mendoza <165741938+luis86129@users.noreply.github.com>
2026-07-04 21:59:52 -0500

    commit test.java

---
+++ Luis/Java/Algoritmos/src/main/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void imprimirHolaMundo() {
+        System.out.println("Hola Mundo");
+    }
+
+    public static void main(String[] args) {
+        imprimirHolaMundo();
+    }
+}
